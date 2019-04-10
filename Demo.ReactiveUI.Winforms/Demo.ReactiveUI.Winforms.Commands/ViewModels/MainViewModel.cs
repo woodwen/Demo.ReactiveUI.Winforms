1f3c6f1d51c9ce4a21065e387b0f47ae7e4ac892
tangwen <tangwen1@kezaihui.com>
2019-04-10 14:50:30 +0800

    解决冲突

---
+++ Demo.ReactiveUI.Winforms/Demo.ReactiveUI.Winforms.Commands/ViewModels/MainViewModel.cs
@@ -0,0 +1,102 @@
+﻿namespace Demo.ReactiveUI.Winforms.Commands.ViewModels
+{
+    using global::ReactiveUI;
+    using System;
+    using System.Reactive.Linq;
+    using System.Windows.Forms;
+
+    public class MainViewModel : ReactiveObject
+    {
+        #region Fields
+
+        private string _applicationTitle;
+        private string _withCanExecuteParameter;
+
+        #endregion Fields
+
+        #region Constructors
+
+        public MainViewModel()
+        {
+            // 设置属性
+            ApplicationTitle = "ReactiveUI Winforms Demo - Commands";
+
+            // 创建无参数命令
+            ParameterlessCommand = ReactiveCommand.Create();
+            ParameterlessCommand.Subscribe(new Action<object>(Parameterless));
+
+            // 使用参数创建命令
+            WithParameterCommand = ReactiveCommand.Create();
+            WithParameterCommand.Subscribe(new Action<object>(WithParameter));
+
+            // 带判断执行创建命令
+            var canExecute = this.WhenAnyValue(vm => vm.WithCanExecuteParameter).Select(s => string.IsNullOrEmpty(s) == false);
+            WithCanExecuteCommand = ReactiveCommand.Create(canExecute);
+            WithCanExecuteCommand.Subscribe(new Action<object>(WithCanExecute));
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string ApplicationTitle
+        {
+            get => _applicationTitle;
+            set => this.RaiseAndSetIfChanged(ref _applicationTitle, value);
+        }
+
+        /// <summary>
+        /// 无参数命令
+        /// </summary>
+        public ReactiveCommand<object> ParameterlessCommand
+        {
+            get;
+        }
+
+        /// <summary>
+        /// 带参数可执行命令
+        /// </summary>
+        public ReactiveCommand<object> WithCanExecuteCommand
+        {
+            get;
+        }
+
+        public string WithCanExecuteParameter
+        {
+            get => _withCanExecuteParameter;
+            set => this.RaiseAndSetIfChanged(ref _withCanExecuteParameter, value);
+        }
+
+        /// <summary>
+        /// 带参数命令
+        /// </summary>
+        public ReactiveCommand<object> WithParameterCommand
+        {
+            get;
+        }
+
+        #endregion Properties
+
+        #region Methods
+
+        private void Parameterless(object message)
+        {
+            MessageBox.Show("You pressed the button!", ApplicationTitle, MessageBoxButtons.OK);
+        }
+
+        private void WithCanExecute(object message)
+        {
+            MessageBox.Show(message.ToString(), ApplicationTitle, MessageBoxButtons.OK);
+        }
+
+        private void WithParameter(object message)
+        {
+            MessageBox.Show(message.ToString(), ApplicationTitle, MessageBoxButtons.OK);
+        }
+
+        #endregion Methods
+    }
+}
---
+++ Demo.ReactiveUI.Winforms/Demo.ReactiveUI.Winforms.Routing/ViewModels/ShellViewModel.cs
@@ -0,0 +1,103 @@
+﻿namespace Demo.ReactiveUI.Winforms.Routing.ViewModels
+{
+    using global::ReactiveUI;
+    using System;
+    using System.Reactive.Linq;
+
+    public class ShellViewModel : ReactiveObject, IScreen
+    {
+        #region Fields
+
+        private string _applicationTitle;
+
+        #endregion Fields
+
+        #region Constructors
+
+        public ShellViewModel()
+        {
+            // 初始化路由
+            Router = new RoutingState();
+            // 初始化属性
+            ApplicationTitle = "ReactiveUI Winforms Demo - Routing";
+            // 初始化命令
+            ShowHomeCommand = ReactiveCommand.Create();
+            ShowHomeCommand.Subscribe(new Action<object>(ShowHome));
+            ShowAboutCommand = ReactiveCommand.Create();
+            ShowAboutCommand.Subscribe(new Action<object>(ShowAbout));
+            ShowContactCommand = ReactiveCommand.Create();
+            ShowContactCommand.Subscribe(new Action<object>(ShowContact));
+            GoBackCommand = ReactiveCommand.Create(Router.NavigateBack.CanExecuteObservable);
+            GoBackCommand.Subscribe(new Action<object>(GoBack));
+
+            Router.NavigateAndReset.Execute(new HomeViewModel());
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        public string ApplicationTitle
+        {
+            get => _applicationTitle;
+            set => this.RaiseAndSetIfChanged(ref _applicationTitle, value);
+        }
+
+        public ReactiveCommand<object> GoBackCommand
+        {
+            get;
+        }
+
+        public RoutingState Router
+        {
+            get;
+        }
+
+        public ReactiveCommand<object> ShowAboutCommand
+        {
+            get;
+        }
+
+        public ReactiveCommand<object> ShowContactCommand
+        {
+            get;
+        }
+
+        public ReactiveCommand<object> ShowHomeCommand
+        {
+            get;
+        }
+
+        #endregion Properties
+
+        #region Methods
+
+        private void GoBack(object obj)
+        {
+            if (Router.NavigationStack.Count > 0)
+            {
+                Router.NavigateBack.Execute(obj);
+            }
+        }
+
+        private void ShowAbout(object obj)
+        {
+            // 导航到AboutViewModel
+            Router.Navigate.Execute(new AboutViewModel());
+        }
+
+        private void ShowContact(object obj)
+        {
+            // 导航到ContactViewModel
+            Router.Navigate.Execute(new ContactViewModel());
+        }
+
+        private void ShowHome(object obj)
+        {
+            // 导航到HomeViewModel
+            Router.Navigate.Execute(new HomeViewModel());
+        }
+
+        #endregion Methods
+    }
+}
